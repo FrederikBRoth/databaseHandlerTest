6801e44c574b8aedb586d545cf842c9d69f4d389
FrederikBRoth <32262672+FrederikBRoth@users.noreply.github.com>
2019-10-10 00:14:50 +0200

    Can add user
    Can read users

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using pleeweasse.Models;
+using System;
+using System.Linq;
 
 namespace pleeweasse
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
+
+            using(var db = new semester3Context())
+            { 
+                //Read all users
+                var user = db.User
+                    .OrderBy(b => b.UserId.First()).ToList();
+                Console.WriteLine(user.ToString());
+
+                foreach (User test in user)
+                {
+                    Console.WriteLine(test.UserId);
+
+                }
+                
+                //Add new user
+                db.Add(new User
+                {
+                    UserId = "fuck dig",
+                    UserLogin = "hjæawdawd",
+                    UserPassword = "FUCK",
+                    UserEmail = "hjælp mig"
+                });
+                db.SaveChanges();
+
+            }
         }
     }
 }
